4fc5d866ca38dc8e5a5ac2f25d43c6d9b4e2fd70
Geiel Peguero <geielpeguero@gmail.com>
2020-11-13 17:35:37 -0400

    add night mode

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let actualWeather = "clear";
 let actualColor = "black";
 let actualVerticalColor = "black-vertical";
 let apiKey = "1ef8550400ece86661dfa0fc0e0af7d7";
+let hour = (new Date()).getHours();
 
 const getWheaterByQuery = async (query) => {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" +  apiKey + "&units=imperial");
@@ -16,17 +17,22 @@ const getWheaterByCoorditates = async ({coords}) => {
 }
 
 const setImageByWeather = (weather) => {
-    console.log(weather);
-    setContentColorByWeather(weather);
+    hour = (new Date()).getHours();
     document.querySelector(".weather-image").classList.remove(actualWeather);
-    document.querySelector(".weather-image").classList.add(weather.toLowerCase());
-    actualWeather = weather.toLowerCase();
+
+    if (hour <= 18) {
+        document.querySelector(".weather-image").classList.add(weather.toLowerCase());
+        actualWeather = weather.toLowerCase();
+    } else {
+        document.querySelector(".weather-image").classList.add(weather.toLowerCase() + "-night");
+        actualWeather = weather.toLowerCase() + "-night";
+    }
 }
 
 const setWeatherContent = (weather) => {
     const temperature = Math.round(weather.main.temp);
     const description = weather.weather[0].description;
-    const weatherContent = "<i class='" + getIcon(description) + "'></i> " + temperature + "°</h1>";
+    const weatherContent = "<i class='" + getIconAndSetColor(weather) + "'></i> " + temperature + "°</h1>";
     const country = weather.name.toUpperCase() + ", " + weather.sys.country;
     document.querySelector(".country").textContent = country;
     document.querySelector(".weather").innerHTML = weatherContent;
@@ -52,118 +58,84 @@ const setWeatherConditions = (weather) => {
     document.querySelector(".conditions").innerHTML = conditionsHTML;
 }
 
-const getIcon = (description) => {
-    let icon = "";
-    switch (description) {
-        case "clear sky":
-            icon = "flaticon-sun";
-            break;
-        case "few clouds":
-            icon = "flaticon-cloudy";
-            break;
-        case "scattered clouds":
-            icon = "flaticon-cloudy-1";
-            break;
-        case "broken clouds":
-            icon = "flaticon-cloudy-1";
-            break;
-        case "overcast clouds":
-            icon = "flaticon-cloudy-1";
-            break;
-        case "shower rain":
-            icon = "flaticon-rain";
-            break;
-        case "heavy intensity rain":
-            icon = "flaticon-rain";
-            break;
-        case "very heavy rain": 
-            icon = "flaticon-rain";
-            break;
-        case "light rain":
-            icon = "flaticon-rain-3";
-            break;
-        case "rain":
-            icon = "flaticon-rain-3";
-            break;
-        case "moderate rain":
-            icon = "flaticon-rain-3";
-            break;
-        case "thunderstorm":
-            icon = "flaticon-thunder";
-            break;
-        case "snow":
-            icon = "flaticon-snowflake";
-            break;
-        case "tornado":
-            icon = "flaticon-tornado"
-            break;
-        default:
-            icon = "flaticon-fog";
-            break;
+const getIconByTime = (iconDay, iconNight) => {
+    hour = (new Date()).getHours();
+    if (hour <= 18) {
+        return iconDay;
+    } else {
+        return iconNight;
     }
-    return icon;
 }
 
-const setContentColorByWeather = (weather) => {
-    
+const getIconAndSetColor = (weather) => {
+    weather = weather.weather[0].main;
     switch (weather) {
         case "Clouds":
-            setColor("black");
-            break;
+            setColor("black", "white");
+            return getIconByTime("flaticon-cloudy", "flaticon-cloudy-2");
         case "Clear":
-            setColor("white");
-            break;
+            setColor("white", "white");
+            return getIconByTime("flaticon-sun", "flaticon-full-moon");
         case "Fog":
-            setColor("black");
-            break;
+            setColor("black", "white");
+            return getIconByTime("flaticon-fog-1", "flaticon-fog-2");
         case "Mist":
-            setColor("white");
-            break;
+            setColor("white", "black");
+            return "flaticon-fog";
         case "Thunderstorm":
-            setColor("white");
-            break;
+            setColor("white", "white");
+            return "flaticon-thunder";
         case "Rain":
-            setColor("white");
-            break;
+            setColor("white", "white");
+            return "flaticon-rain-1";
         case "Drizzle":
-            setColor("white");
-            break;
+            setColor("white", "white");
+            return "flaticon-rain-3";
         case "Squall":
-            setColor("white");
-            break;
+            setColor("white", "white");
+            return "flaticon-rain";
         case "Snow":
-            setColor("black");
-            break;
+            setColor("black", "white");
+            return "flaticon-snowflake";
         case "Smoke":
-            setColor("white");
-            break;
+            setColor("white", "black");
+            return "flaticon-fog";
         case "Haze":
-            setColor("black");
-            break;
+            setColor("black", "white");
+            return getIconByTime("flaticon-fog-1", "flaticon-fog-2");
         case "Dust":
-            setColor("black");
-            break;
+            setColor("black", "white");
+            return getIconByTime("flaticon-fog-1", "flaticon-fog-2");
         case "Sand":
-            setColor("white");
-            break;
+            setColor("white", "black");
+            return getIconByTime("flaticon-fog-1", "flaticon-fog-2");
         case "Ash":
-            setColor("black");
-            break;
+            setColor("black", "white");
+            return "flaticon-fog";
         case "Tornado":
-            setColor("white");
-            break;
+            setColor("white", "black");
+            return "flaticon-tornado";
         default:
             break;
     }
 }
 
-const setColor = (color) => {
+const setColor = (colorDay, colorNight) => {
+    hour = (new Date()).getHours();
     document.querySelector(".container-fluid").classList.remove(actualColor);
-    document.querySelector(".container-fluid").classList.add(color);
     document.querySelector(".vertical").classList.remove(actualVerticalColor);
-    document.querySelector(".vertical").classList.add(color + "-vertical");
-    actualColor = color;
-    actualVerticalColor = color + "-vertical";
+
+    if (hour <= 18) {
+        document.querySelector(".container-fluid").classList.add(colorDay);
+        document.querySelector(".vertical").classList.add(colorDay + "-vertical");
+        actualColor = colorDay;
+        actualVerticalColor = colorDay + "-vertical";
+    } else {
+        document.querySelector(".container-fluid").classList.add(colorNight);
+        document.querySelector(".vertical").classList.add(colorNight + "-vertical");
+        actualColor = colorNight;
+        actualVerticalColor = colorNight + "-vertical";
+    }
 }
 
 const showWeatherByLocation = (position) => {
